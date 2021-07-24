c4d6c947703aaf8cde3fa934772aadbbade61d73
mahdimorasmmb <67733580+mahdimorasmmb@users.noreply.github.com>
2021-07-24 16:58:19 +0430

    replase function

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
+const replaceInput = document.querySelector("#replaceInput");
+const SearchInput = document.querySelector("#SearchInput")
+const Text = document.querySelector("#text");
+let result= []
+let reg 
+
 const finde = () => {
-    const SearchInput = document.querySelector("#SearchInput")
-    const Text = document.querySelector("#text");
-    let result= []
+   
     let strSearch;
     if(SearchInput.value){
         strSearch = SearchInput.value;
     } else {
         alert("Please enter a search term")
     }
-    let reg  = new RegExp(`${strSearch}`,"gi")
+    reg = new RegExp(`${strSearch}`,"gi")
     Text.innerHTML = Text.innerHTML.replace(reg,`<mark>${strSearch}</mark>`)
     
-    
-    
-    
+}
 
+const replace =() => { 
+    let strreplace ;
+    if(replaceInput.value){
+        strreplace = replaceInput.value
+    } else {
+        alert("Please enter a replac text")
+    }
+    Text.innerHTML = Text.innerHTML.replace(reg,strreplace)
 }
