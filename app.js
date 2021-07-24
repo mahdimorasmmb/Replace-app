6fc7f95962a1e3a6b1e6ddd2db9f4820fa05ffaf
mahdimorasmmb <67733580+mahdimorasmmb@users.noreply.github.com>
2021-07-24 21:25:07 +0430

    css and html update

--- app.js
+++ app.js
@@ -1,28 +1,36 @@
 const replaceInput = document.querySelector("#replaceInput");
 const SearchInput = document.querySelector("#SearchInput")
 const Text = document.querySelector("#text");
-let result= []
-let reg 
+
 
 const finde = () => {
-   
+    let reg;
     let strSearch;
-    if(SearchInput.value){
+    let stokText = Text.innerHTML;
+    if (SearchInput.value) {
         strSearch = SearchInput.value;
+        reg = new RegExp(`${strSearch}`, "gi");
+        Text.innerHTML = Text.innerHTML.replace(reg, `<mark>${strSearch}</mark>`);
+        if(stokText === Text.innerHTML){
+            alert("Not found string")
+        } else{
+            setTimeout(()=>{
+                reg = new RegExp(`<mark>${strSearch}</mark>`, "gi");
+                Text.innerHTML = Text.innerHTML.replace(reg,`${strSearch}`)
+            },20000)
+        }
     } else {
         alert("Please enter a search term")
     }
-    reg = new RegExp(`${strSearch}`,"gi")
-    Text.innerHTML = Text.innerHTML.replace(reg,`<mark>${strSearch}</mark>`)
-    
+   
 }
 
-const replace =() => { 
-    let strreplace ;
-    if(replaceInput.value){
+const replace = () => {
+    let strreplace;
+    if (replaceInput.value) {
         strreplace = replaceInput.value
     } else {
         alert("Please enter a replac text")
     }
-    Text.innerHTML = Text.innerHTML.replace(reg,strreplace)
+    Text.innerHTML = Text.innerHTML.replace(reg, strreplace)
 }
